02f303b92624034b0ceb1a72ab83d02beb59703b
Colin Rafferty <colin@rafferty.net>
2025-12-01 14:46:53 -0500

    LocalRegionView

---
+++ eBirdAlert/eBirdAlert/LocalRegionView.swift
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (C) 2025 Colin Rafferty <colin@rafferty.net>
+
+import MapKit
+import Schema
+import SwiftUI
+
+struct LocalRegionView: View {
+    var regionService: any eBirdRegionService
+    @Environment(LocationService.self) var locationService
+    @State var regions: [eBirdRegionInfo] = []
+    @State var showError: Bool = false
+    @State var error: eBirdServiceError? = nil
+    @State var selectedCode: String?
+
+    var body: some View {
+        VStack {
+            if regions.isEmpty {
+                Text("loading regions...")
+                ProgressView()
+            } else {
+                mapView
+            }
+        }
+        .task {
+            await load()
+        }
+        .refreshable {
+            await load()
+        }
+        .alert(isPresented: $showError, error: error) {}
+    }
+
+    private var mapView: some View {
+        VStack {
+            List(regions, selection: $selectedCode) {
+                Text($0.result)
+            }
+            Map {
+                ForEach(regions) { info in
+                    if let bounds = info.bounds {
+                        box(for: bounds, with: info.code == selectedCode)
+                    }
+                }
+                if let location = locationService.location {
+                    Marker(coordinate: location.coordinate) {}
+                }
+            }
+        }
+    }
+
+    private func box(for bounds: eBirdRegionInfo.Bounds,
+                     with selected: Bool) -> some MapContent
+    {
+        MapPolyline(coordinates: bounds.coordinates)
+            .stroke(selected ? .blue : .red,
+                    lineWidth: selected ? 5 : 2)
+    }
+
+    private func load() async {
+        do {
+            guard let location = locationService.location else {
+                throw eBirdServiceError.noLocation
+            }
+            try await regions =
+                regionService.getRegions(near: location).sorted {
+                    $0.distance2(location) ?? 0 < $1.distance2(location) ?? 0
+                }
+        } catch {
+            self.error = eBirdServiceError.from(error)
+            showError = true
+        }
+    }
+}
--- eBirdAlert/eBirdAlert/eBirdRegionInfo+Ext.swift
+++ eBirdAlert/eBirdAlert/eBirdRegionInfo+Ext.swift
@@ -8,6 +8,11 @@ extension eBirdRegionInfo {
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
+
+    /** return square of the distance */
+    func distance2(_ location: CLLocation) -> Double? {
+        bounds?.distance2(location)
+    }
 }
 
 extension eBirdRegionInfo.Bounds {
@@ -19,5 +24,14 @@ extension eBirdRegionInfo.Bounds {
             CLLocationCoordinate2D(latitude: maxY, longitude: minX),
             CLLocationCoordinate2D(latitude: minY, longitude: minX),
         ]
+    }
+
+    /** return square of the distance */
+    func distance2(_ location: CLLocation) -> Double {
+        // https://stackoverflow.com/a/18157551
+        let c = location.coordinate
+        let dx = max(minX - c.longitude, 0, c.longitude - maxX)
+        let dy = max(minY - c.latitude, 0, c.latitude - maxY)
+        return dx * dx + dy * dy
     }
 }
