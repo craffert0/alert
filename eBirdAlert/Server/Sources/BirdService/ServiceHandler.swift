297dca6fc0822e8f22b21ef9f256e59cb3871ef9
Colin Rafferty <colin@rafferty.net>
2026-01-01 22:54:01 -0500

    implement fake /api/v1/notableQuery in the server

--- eBirdAlert/Server/Sources/BirdService/ServiceHandler.swift
+++ eBirdAlert/Server/Sources/BirdService/ServiceHandler.swift
@@ -3,7 +3,32 @@
 
 import AlertAPI
 
+extension Components.Schemas.Range {
+    func dump() {
+        switch self {
+        case let .RegionCode(code):
+            print("region: \(code)")
+        case let .Circle(c):
+            print("\(c.location.lat), \(c.location.lng): \(c.radius) \(c.units.rawValue)")
+        }
+    }
+}
+
 struct ServiceHandler: APIProtocol {
+    func postNotableQuery(_ input: Operations.PostNotableQuery.Input) async throws
+        -> Operations.PostNotableQuery.Output
+    {
+        // TODO: do something with this!
+        switch input.body {
+        case let .json(q):
+            print("\(q.userToken)/\(q.deviceId)")
+            print("\(q.daysBack) day(s) back")
+            q.range.dump()
+            print(q.results.joined(separator: ", "))
+        }
+        return .accepted
+    }
+
     func getYo(_: Operations.GetYo.Input) async throws
         -> Operations.GetYo.Output
     {
