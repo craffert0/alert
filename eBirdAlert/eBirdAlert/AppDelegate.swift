26461a3fc7a4bb2fb996c99fe9222ac62aaf80fb
Colin Rafferty <colin@rafferty.net>
2026-01-10 21:58:19 -0500

    app receives remote notifications and hack up getting device token

--- eBirdAlert/eBirdAlert/AppDelegate.swift
+++ eBirdAlert/eBirdAlert/AppDelegate.swift
@@ -24,11 +24,29 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 
 extension AppDelegate: UIApplicationDelegate {
     func application(
-        _: UIApplication,
+        _ application: UIApplication,
         didFinishLaunchingWithOptions _:
         [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         UNUserNotificationCenter.current().delegate = self
+        application.registerForRemoteNotifications()
         return true
+    }
+
+    @MainActor
+    func application(
+        _: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        let hex = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("registered: \(hex)")
+    }
+
+    @MainActor
+    func application(
+        _: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: any Error
+    ) {
+        print("failed: \(error)")
     }
 }
